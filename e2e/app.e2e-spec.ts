34b013f737caadb171c5ef96b454082727eef105
Angular CLI <angular-cli@angular.io>
2017-07-20 06:25:52 -0500

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { CampComfyPage } from './app.po';
+
+describe('camp-comfy App', () => {
+  let page: CampComfyPage;
+
+  beforeEach(() => {
+    page = new CampComfyPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
