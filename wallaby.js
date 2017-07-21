15c0b08b3f32bd447e8932729044be362c256b41
Dennis Moon <dmoon.comfycoder@gmail.com>
2017-07-21 16:14:57 -0500

    Add references to the jquery, tether, and bootstrap JavaScript files to the wallaby.js file

--- wallaby.js
+++ wallaby.js
@@ -15,15 +15,15 @@ module.exports = function (wallaby) {
 
     module: {
       loaders: [
-        {test: /\.css$/, loader: 'raw-loader'},
-        {test: /\.html$/, loader: 'raw-loader'},
-        {test: /\.ts$/, loader: '@ngtools/webpack', include: /node_modules/, query: {tsConfigPath: 'tsconfig.json'}},
-        {test: /\.js$/, loader: 'angular2-template-loader', exclude: /node_modules/},
-        {test: /\.json$/, loader: 'json-loader'},
-        {test: /\.styl$/, loaders: ['raw-loader', 'stylus-loader']},
-        {test: /\.less$/, loaders: ['raw-loader', 'less-loader']},
-        {test: /\.scss$|\.sass$/, loaders: ['raw-loader', 'sass-loader']},
-        {test: /\.(jpg|png)$/, loader: 'url-loader?limit=128000'}
+        { test: /\.css$/, loader: 'raw-loader' },
+        { test: /\.html$/, loader: 'raw-loader' },
+        { test: /\.ts$/, loader: '@ngtools/webpack', include: /node_modules/, query: { tsConfigPath: 'tsconfig.json' } },
+        { test: /\.js$/, loader: 'angular2-template-loader', exclude: /node_modules/ },
+        { test: /\.json$/, loader: 'json-loader' },
+        { test: /\.styl$/, loaders: ['raw-loader', 'stylus-loader'] },
+        { test: /\.less$/, loaders: ['raw-loader', 'less-loader'] },
+        { test: /\.scss$|\.sass$/, loaders: ['raw-loader', 'sass-loader'] },
+        { test: /\.(jpg|png)$/, loader: 'url-loader?limit=128000' }
       ]
     },
 
@@ -38,20 +38,23 @@ module.exports = function (wallaby) {
 
   return {
     files: [
-      {pattern: 'src/**/*.ts', load: false},
-      {pattern: 'src/**/*.d.ts', ignore: true},
-      {pattern: 'src/**/*.css', load: false},
-      {pattern: 'src/**/*.less', load: false},
-      {pattern: 'src/**/*.scss', load: false},
-      {pattern: 'src/**/*.sass', load: false},
-      {pattern: 'src/**/*.styl', load: false},
-      {pattern: 'src/**/*.html', load: false},
-      {pattern: 'src/**/*.json', load: false},
-      {pattern: 'src/**/*spec.ts', ignore: true}
+      { pattern: 'node_modules/jquery/dist/jquery.slim.min.js', load: true },
+      { pattern: 'node_modules/tether/dist/js/tether.min.js', load: true },
+      { pattern: 'node_modules/bootstrap/dist/js/bootstrap.min.js', load: true },
+      { pattern: 'src/**/*.ts', load: false },
+      { pattern: 'src/**/*.d.ts', ignore: true },
+      { pattern: 'src/**/*.css', load: false },
+      { pattern: 'src/**/*.less', load: false },
+      { pattern: 'src/**/*.scss', load: false },
+      { pattern: 'src/**/*.sass', load: false },
+      { pattern: 'src/**/*.styl', load: false },
+      { pattern: 'src/**/*.html', load: false },
+      { pattern: 'src/**/*.json', load: false },
+      { pattern: 'src/**/*spec.ts', ignore: true }
     ],
 
     tests: [
-      {pattern: 'src/**/*spec.ts', load: false}
+      { pattern: 'src/**/*spec.ts', load: false }
     ],
 
     testFramework: 'jasmine',
@@ -59,7 +62,7 @@ module.exports = function (wallaby) {
     compilers: {
       '**/*.ts': wallaby.compilers.typeScript(compilerOptions)
     },
-    
+
     middleware: function (app, express) {
       var path = require('path');
       app.use('/favicon.ico', express.static(path.join(__dirname, 'src/favicon.ico')));
