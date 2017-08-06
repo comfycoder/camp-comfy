4825a3ce046d72d710681aab9ad75e584d0d44a5
Dennis Moon <dmoon.comfycoder@gmail.com>
2017-08-06 14:35:11 -0500

    Added page header and footer components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,20 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { PageFooterComponent } from './page-footer/page-footer.component';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
   ],
   declarations: [
-
+    PageHeaderComponent,
+    PageFooterComponent
   ],
   exports: [
-
+    PageHeaderComponent,
+    PageFooterComponent
   ]
 })
 export class SharedModule {
